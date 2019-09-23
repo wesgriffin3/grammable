15d978a4a65d11f9fe9cc5a3fd8c1e9cbfc52829
Wes Griffin <typicalgames@gmail.com>
2019-09-23 01:06:56 +0000

    add validations for pictures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,5 +59,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :controller
+  include ActionDispatch::TestProcess
   config.include Devise::Test::ControllerHelpers, type: :view
 end
